f59d57dfc6b2a016d3603d3cef6080a5f8fbcb92
austerj <37388465+austerj@users.noreply.github.com>
2022-12-01 02:21:49 +0100

    test: assert_exists override

--- tests/test_overrides.py
+++ tests/test_overrides.py
@@ -2,7 +2,7 @@ import pyteal as pt
 
 import pyteal_maybenot as ptmn
 
-from .utils import compile, format_teal
+from .utils import compile, compile_popped, format_teal
 
 JUMP_TO_GET_BRANCH_IF_TRUE = "bnz main_l2"
 GET_BRANCH = "main_l2:"
@@ -34,7 +34,7 @@ def test_no_cache_on_exists_override():
 
 
 def test_no_cache_on_get_override():
-    # test that no caching happens when explicitly disabling loading on getter
+    # test that cache isn't loaded when explicitly disabling loading on getter
     key = ptmn.ExAppGlobal(pt.Txn.applications[1], pt.Bytes("key"))
     teal = compile(pt.If(key.exists(), key.get(load=False), pt.Int(0)))
     assert teal == format_teal(
@@ -53,6 +53,22 @@ def test_no_cache_on_get_override():
         app_global_get_ex
         assert
         main_l3:
+        return
+        """
+    )
+
+
+def test_no_assert_on_get_override():
+    # test that assert isn't performed when explicitly disabling asserting existence on getter
+    teal = compile_popped(ptmn.ExAppGlobal(pt.Txn.applications[1], pt.Bytes("key")).get(assert_exists=False))
+    assert teal == format_teal(
+        f"""
+        txna Applications 1
+        byte \"key\"
+        app_global_get_ex
+        pop
+        pop
+        int 1
         return
         """
     )
